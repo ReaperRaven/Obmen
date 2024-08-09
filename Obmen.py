9f25461a34f3d197c91d99c6c2b664e266e871c0
ReaperRaven <chekmarev.evgeniy.88@mail.ru>
2024-08-09 23:08:44 +0300

    В метку добавлены названия валют

--- Obmen.py
+++ Obmen.py
@@ -1,13 +1,18 @@
 import requests
 import json
 from tkinter import *
-from tkinter import  messagebox as mb
+from tkinter import messagebox as mb
 from tkinter import ttk
+
+
+def update_c_label(event):
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
 
 def exchange():
     code = combobox.get()
 #    code = entry.get() #для ввода валюты в окно
-
     if code:
         try:
             response = requests.get('https://open.er-api.com/v6/latest/USD')
@@ -15,7 +20,8 @@ def exchange():
             data = response.json()
             if code in data['rates']:
                 exchange_rate = data['rates'][code]
-                mb.showinfo('Курс обмена', f'Курс: {exchange_rate:.2f} {code} за 1 доллар')
+                c_name = cur[code]
+                mb.showinfo('Курс обмена', f'Курс: {exchange_rate:.2f} {c_name} за 1 доллар')
             else:
                 mb.showerror('Ошибка!', f'Валюта {code} не найдена!')
         except Exception as e:
@@ -24,17 +30,34 @@ def exchange():
         mb.showwarning('Внимание', 'Введите код валюты!')
 
 
+cur = {
+    'RUB': 'Российский рубль',
+    'EUR': 'Евро',
+    'GBP': 'Британский фунт стерлингов',
+    'JPY': 'Японская йена',
+    'CNY': 'Китайский юань',
+    'KZT': 'Казахский тенге',
+    'UZS': 'Узбекский сум',
+    'CHF': 'Швейцарский франк',
+    'AED': 'Дирхам ОАЭ',
+    'CAD': 'Канадский доллар'}
+
+
 window = Tk()
 window.title('Курсы обмена валют')
 window.geometry('360x180')
 
 Label(text='Выберите код валюты').pack(padx=10, pady=10) #выбор валюты из списка
-cur = ['RUB','EUR','GBP','JPY','CNY','KZT','UZS','CHF','AED','CAD',]
-combobox = ttk.Combobox(values=cur)
+
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind('<<ComboboxSelected>>', update_c_label)
 
 # entry = Entry() #ввод валюты
 #entry.pack(padx=10, pady=10)
+
+c_label = ttk.Label(text='')
+c_label.pack(padx=10, pady=10)
 
 Button(text='Получит курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
